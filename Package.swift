b8bc77051df20076650c439e952e7e90c3e2e619
Brian Boyle <brian.boyle@intercom.io>
2021-01-13 23:40:49 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Intercom",
-            url: "https://github.com/intercom/spm-test/releases/download/3.0.0/Intercom.xcframework.zip",
-            checksum: "bf79a0cef9117a9ca05483d8166607c43f5e0e9fcbf45aa05a20fbafc58bd5a4"
+            url: "https://github.com/intercom/intercom-ios/releases/download/9.1.0/Intercom.xcframework.zip",
+            checksum: "1a36b3c4e8990d0860c68b44e11fa43cbaec98b43e6349f7bfe0629789c19098"
         ),
     ]
 )
